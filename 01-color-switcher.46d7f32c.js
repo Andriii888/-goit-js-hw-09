23b38e7639d9731c75651fe345d15f968aec2a98
Andriii888 <106496840+Andriii888@users.noreply.github.com>
2022-11-28 21:45:34 +0000

    Deploying to gh-pages from @ Andriii888/goit-js-hw-09@353a2e8629fd2dc12d21d31c37c21e2f6b7841db 🚀

---
+++ 01-color-switcher.46d7f32c.js
@@ -0,0 +1,2 @@
+const t={bodyRef:document.querySelector("body"),btnStartRef:document.querySelector("[data-start]"),btnStopRef:document.querySelector("[data-stop]")};t.btnStartRef.addEventListener("click",(function(){e=setInterval(n,1e3),t.btnStartRef.disabled=!0})),t.btnStopRef.addEventListener("click",(function(){t.btnStartRef.disabled=!1,clearInterval(e)}));let e=null;function n(){let e=`#${Math.floor(16777215*Math.random()).toString(16)}`;return t.bodyRef.style.backgroundColor=`${e}`}
+//# sourceMappingURL=01-color-switcher.46d7f32c.js.map
