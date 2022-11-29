ee42fbc4aed713efa52c1702599cc0a4ab49e705
Andriii888 <106496840+Andriii888@users.noreply.github.com>
2022-11-30 00:14:48 +0300

    add2taskhaf

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,18 +1,57 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
+const inputForTimeRef = document.querySelector('#datetime-picker');
+const startBtnref = document.querySelector('button[data-start]');
+const timerTextDaysRef = document.querySelector('[data-days]');
+const timerTextHoursRef = document.querySelector('[data-hours]');
+const timerTextMinutesRef = document.querySelector('[data-minutes]');
+const timerTextSecondsRef = document.querySelector('[data-seconds]');
+
+
+startBtnref.disabled = true;
+
+let timer = null;
+let currentTime = Date.now();
+
+// setInterval(() => { console.log(currentTime) }, 1000);
+let selectedTime = 0;
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
+    const timeInMSeconds =new Date(selectedDates[0]).getTime();
+    if (timeInMSeconds <= currentTime) {
+      window.alert("Please choose a date in the future");
+    } else {
+      startBtnref.disabled = false;
+      selectedTime = timeInMSeconds;
+        }
+    },
 };
-const inputForTimeRef = document.querySelector('#datetime-picker');
-const startBtnref = document.querySelector('button[data-start]');
-console.log(startBtnref);
+
+startBtnref.addEventListener('click', onStartBtn);
+
+function onStartBtn() {
+  timer = setInterval(() => {
+    let currentTime = new Date();
+    let timerTime = selectedTime - currentTime;
+    if (timerTime < 0) {
+      return clearInterval(timer);
+    }
+    
+    const { days, hours, minutes, seconds } = convertMs(timerTime);
+    timerTextDaysRef.textContent= `${days}`;
+    timerTextHoursRef.textContent= `${hours}`;
+    timerTextMinutesRef.textContent= `${minutes}`;
+    timerTextSecondsRef.textContent= `${seconds}`;
+
+
+  }, 1000)
+};
 
 flatpickr(inputForTimeRef, options);
 
