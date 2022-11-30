eca4274d5977a9f59c6ed76dc168e6f288aa739f
Andriii888 <106496840+Andriii888@users.noreply.github.com>
2022-12-01 01:52:41 +0300

    add3task50%

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+import Notiflix from 'notiflix';
+
+const formRef = document.querySelector('.form');
+
+let obj = {};
+formRef.addEventListener('change', onChangePromise);
+function onChangePromise(e) { 
+  const name = e.target.name;
+  const value = e.target.value;
+  obj[name] = value;
+  console.log(obj)
+};
+
+formRef.addEventListener('submit', createPromise());
+
+function createPromise (position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    const timerId = setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay })
+      }
+    }, obj.delay);
+  })
+  
+};
+
+createPromise(obj)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
